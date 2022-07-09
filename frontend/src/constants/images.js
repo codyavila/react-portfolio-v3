b075d10ba64c0835cff2b07977c947534c95416c
sanderscody26 <sanderscody26@gmail.com>
2022-07-09 14:50:01 -0400

    Color pallet change, new logo, new effects

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -25,7 +25,7 @@ import about04 from '../assets/about04.png'
 
 import profile from '../assets/profile.png'
 import circle from '../assets/circle.svg'
-import logo from '../assets/Cody.png'
+import logo from '../assets/logo3.png'
 
 import adidas from '../assets/adidas.png'
 import amazon from '../assets/amazon.png'
